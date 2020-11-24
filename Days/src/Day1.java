9df98a3bc3d5bd6650a273521723ca1c7dc6ed1b
lisahalman <61053821+lisahalman@users.noreply.github.com>
2020-11-24 14:44:19 +0100

    test vanaf laptop

--- Days/src/Day1.java
+++ Days/src/Day1.java
@@ -3,5 +3,6 @@ public class Day1 {
     public static void main(String[] args) {
         System.out.println("test voor dag 1");
         System.out.println("test commit vanaf pc");
+        System.out.println("test weer vanaf laptop");
     }
 }
