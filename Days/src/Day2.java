d69a2c4b9123efc2eb8112f0df100a38e74deff3
lisahalman <61053821+lisahalman@users.noreply.github.com>
2020-12-02 08:04:59 +0100

    day 2 complete

--- Days/src/Day2.java
+++ Days/src/Day2.java
@@ -1,2 +1,49 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
 public class Day2 {
+
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(new File("Day2Input.txt").toPath());
+        int count = 0;
+        int newCount = 0;
+        for (int i = 0; i < lines.size(); i++) {
+            String[] splittedOnSpace = lines.get(i).split(" ");
+
+            //numbers
+            String numbers = splittedOnSpace[0];
+            String[] numbersSplittedOnDash = numbers.split("-");
+            int lowest = Integer.parseInt(numbersSplittedOnDash[0]);
+            int highest = Integer.parseInt(numbersSplittedOnDash[1]);
+
+            //letter
+            char letter = splittedOnSpace[1].charAt(0);
+
+            //password
+            String password = splittedOnSpace[2];
+
+            //logics
+            int letterCount = 0;
+            for (char l : password.toCharArray()) {
+                if (l == letter) {
+                    letterCount++;
+                }
+            }
+            if (letterCount >= lowest & letterCount <= highest) {
+                count++;
+            }
+
+            //logics part two
+            char firstLetter = password.charAt(lowest - 1);
+            char lastLetter = password.charAt(highest - 1);
+            if (letter == firstLetter ^ letter == lastLetter) {
+                newCount++;
+            }
+        }
+        System.out.println("Count part 1: " + count);
+        System.out.println("Count part 2: " + newCount);
+    }
+
 }
